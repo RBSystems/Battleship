5d422f6f04fb788e9ad281b325b0a42992f698d1
Wayne George <wayne@lucidpartnership.co.uk>
2018-05-21 16:22:43 +0100

    First commit

---
+++ Battleship/Battleship/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Battleship")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Battleship")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
